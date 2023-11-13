b851bb13a42fd9a142d99c8440a19cd10b40ecdf
Adrian Suciu <adrian.suciu@analog.com>
2023-11-13 13:49:29 +0000

    added init.sh and deploy.sh
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,44 @@
 #!/bin/bash
 
-DIR=$PWD
-cd ~
-rm -rf m2kirl
-mkdir m2kirl
-cp -R DIR/* ~/m2kirl
+SCOPY_DECODER_LOCATION=/var/lib/flatpak/app/org.adi.Scopy/arm/master/2eb23ee2c0bbd73478c4c8d7720ae584df8332dbf94a4e2ee5d4a9dfbf36b935/files/share/libsigrokdecode/decoders/
+SIGROK_CLI_DECODER_LOCATION=/usr/share/libsigrokdecode/decoders/
+WORKDIR=/home/analog/m2kirl
+ZIPFILE=/home/analog/m2kirl.zip
+SRCDIR=/home/analog/tmp
+
+tar xvf $ZIPFILE
+
+echo -- creating working dir
+mkdir -p $WORKDIR
+cp -R $SRCDIR/m2kirl-src/* $WORKDIR/
+
+echo -- uninstalling sigrok-cli libsigrokdecode
+sudo apt-get -y remove libsigrokdecode-dev sigrok-cli
+
+echo -- copying devicetree
+sudo cp $WORKDIR/dt/config.txt /boot/
+sudo cp $WORKDIR/dt/rpi-ad5592r-m2kirl.dtbo /boot/overlays
+
+echo -- installing libm2k
+cd $SRCDIR/libm2k/build
+sudo make install
+echo -- installing pyadi-iio
+cd $SRCDIR/pyadi-iio
+pip3 install .
+echo -- installing libsigrokdecode
+cd $SRCDIR/libsigrokdecode
+sudo make install
+echo -- installing sigrok-cli
+cd $SRCDIR/sigrok-cli
+sudo make install
+
+
+echo -- copying decoder to scopy
+sudo cp -R $WORKDIR/decoder/ad5592r $SCOPY_DECODER_LOCATION
+
+echo -- copying decoder to sigrok-cli
+sudo cp -R $WORKDIR/decoder/ad5592r $SIGROK_CLI_DECODER_LOCATION
+
+echo -- setting default keyboard to US
+sudo cp $WORKDIR/res/keyboard /etc/default/
 
--- init.sh
+++ init.sh
@@ -1,1 +1,43 @@
 #!/bin/bash
+
+M2KIRL_REPO=https://github.com/adisuciu/m2kirl 
+STAGING_DIR=/home/analog/tmp
+OUTPUT_ZIP=/home/analog/m2kirl.zip
+
+echo -- cloning m2kirl
+mkdir -p $STAGING_DIR
+cd $STAGING_DIR
+git clone $M2KIRL_REPO m2kirl-src
+
+echo -- installing libm2k
+cd $STAGING_DIR
+git clone https://github.com/analogdevicesinc/libm2k
+cd libm2k
+mkdir build && cd build
+cmake -DENABLE_TOOLS=ON -DENABLE_PYTHON=ON ../
+make
+
+echo -- installing pyadi-iio
+cd $STAGING_DIR
+git clone https://github.com/analogdevicesinc/pyadi-iio
+
+echo -- installing libsigrokdecode
+cd $STAGING_DIR
+git clone https://github.com/sigrokproject/libsigrokdecode
+cd libsigrokdecode
+./autogen.sh
+./configure
+make
+
+echo -- installing sigrok-cli
+cd $STAGING_DIR
+git clone https://github.com/sigrokproject/sigrok-cli
+cd sigrok-cli
+./autogen.sh
+./configure
+make
+
+echo -- building zip
+tar -cf $OUTPUT_ZIP $STAGING_DIR
+
+
