6a43c0f3f59af233e2d64981bd75b9f23770293e
Adrian Suciu <adrian.suciu@analog.com>
2023-11-13 10:23:25 +0000

    bash: cleaned up red/green scripts
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- bash/green.sh
+++ bash/green.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
+
 iio_attr -u ip:127.0.0.1 -q -c ad5592r -o voltage3 raw 4095
-echo voltage3 - 4095
-#m2kcli analog-in ip:192.168.2.1 -v channel=0 raw=0
 iio_attr -u ip:127.0.0.1 -q -c ad5592r -o voltage2 raw 0
-echo voltage2 0
-#m2kcli analog-in ip:192.168.2.1 -v channel=0 raw=0
