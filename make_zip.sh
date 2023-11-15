f44a6759d4c172b8f1ffdff8b9d0ee8e65431b87
Adrian Suciu <adrian.suciu@analog.com>
2023-11-15 13:52:15 +0000

    make_zip: pinned library versions
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- make_zip.sh
+++ make_zip.sh
@@ -4,6 +4,11 @@ M2KIRL_REPO=https://github.com/adisuciu/m2kirl
 STAGING_DIR=/home/analog/tmp
 OUTPUT_ZIP=/home/analog/m2kirl.zip
 
+LIBM2K_VER=7b31a3d
+PYADI_VER=8bcfac1
+SIGROKDECODE_VER=0c35c5c
+SIGROKCLI_VER=9d9f7b8
+
 echo -- cloning m2kirl
 mkdir -p $STAGING_DIR
 cd $STAGING_DIR
@@ -13,6 +18,7 @@ echo -- installing libm2k
 cd $STAGING_DIR
 git clone https://github.com/analogdevicesinc/libm2k
 cd libm2k
+git reset --hard $LIBM2K_VER 
 mkdir build && cd build
 cmake -DENABLE_TOOLS=ON -DENABLE_PYTHON=ON ../
 make
@@ -20,11 +26,14 @@ make
 echo -- installing pyadi-iio
 cd $STAGING_DIR
 git clone https://github.com/analogdevicesinc/pyadi-iio
+cd pyadi-iio
+git reset --hard $PYADI_VER
 
 echo -- installing libsigrokdecode
 cd $STAGING_DIR
 git clone https://github.com/sigrokproject/libsigrokdecode
 cd libsigrokdecode
+git reset --hard $SIGROKDECODE_VER
 ./autogen.sh
 ./configure
 make
@@ -33,6 +42,7 @@ echo -- installing sigrok-cli
 cd $STAGING_DIR
 git clone https://github.com/sigrokproject/sigrok-cli
 cd sigrok-cli
+git reset --hard $SIGROKCLI_VER
 ./autogen.sh
 ./configure
 make
